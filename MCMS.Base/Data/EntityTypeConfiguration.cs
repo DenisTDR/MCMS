cd50aec3f849265a56cb92813008deab0caa758c
Denis TDR <denis.troncota@ligaac.ro>
2020-06-22 20:20:34 +0300

    remove 'Entity' from db table names

--- MCMS.Base/Data/EntityTypeConfiguration.cs
+++ MCMS.Base/Data/EntityTypeConfiguration.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 using MCMS.Base.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -9,7 +11,30 @@ namespace MCMS.Base.Data
     {
         public virtual void Configure(EntityTypeBuilder<T> builder)
         {
+            builder.ToTable(GetTableName());
+
             builder.HasKey(e => e.Id);
+        }
+        public virtual string GetTableName()
+        {
+            if (typeof(T).GetCustomAttributes(true).LastOrDefault(attr => attr is TableAttribute) is
+                TableAttribute toTableAttribute)
+            {
+                return toTableAttribute.Name;
+            }
+
+            var entityName = typeof(T).Name;
+            if (entityName.EndsWith("Entity"))
+            {
+                entityName = entityName.Substring(0, entityName.Length - 6);
+            }
+
+            if (entityName.EndsWith("Model"))
+            {
+                entityName = entityName.Substring(0, entityName.Length - 5);
+            }
+
+            return entityName;
         }
     }
 }

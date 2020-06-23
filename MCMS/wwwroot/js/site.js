396e9e1f427808ae63ace02ededb56026497001d
Denis TDR <denis.troncota@ligaac.ro>
2020-06-23 05:50:39 +0300

    fix formatDate js func + delete title

--- MCMS/wwwroot/js/site.js
+++ MCMS/wwwroot/js/site.js
@@ -8,7 +8,7 @@ function formatDate(date, separator = '/') {
         date = new Date(date);
     }
     var day = date.getDate();
-    var month = date.getMonth();
+    var month = date.getMonth() + 1;
     if (day < 10) day = "0" + day;
     if (month < 10) month = "0" + month;
     return day + separator + month + separator + date.getFullYear();

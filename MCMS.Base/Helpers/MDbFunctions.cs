bdb647c4ebc2489137c6a6706cf8f9d2eab38b54
TDR <tdr@tdrs.ro>
2021-02-07 11:00:41 +0200

    YOLO

--- MCMS.Base/Helpers/MDbFunctions.cs
+++ MCMS.Base/Helpers/MDbFunctions.cs
@@ -19,6 +19,21 @@ namespace MCMS.Base.Helpers
         public static string Concat(string arg1, string arg2, string arg3, string arg4, string arg5)
             => throw new InvalidOperationException($"{nameof(Concat)} cannot be called client side");
 
+        public static string Concat(string arg1, string arg2, string arg3, string arg4, string arg5, string arg6)
+            => throw new InvalidOperationException($"{nameof(Concat)} cannot be called client side");
+
+        public static string Concat(string arg1, string arg2, string arg3, string arg4, string arg5, string arg6,
+            string arg7)
+            => throw new InvalidOperationException($"{nameof(Concat)} cannot be called client side");
+
+        public static string Concat(string arg1, string arg2, string arg3, string arg4, string arg5, string arg6,
+            string arg7, string arg8)
+            => throw new InvalidOperationException($"{nameof(Concat)} cannot be called client side");
+
+        public static string Concat(string arg1, string arg2, string arg3, string arg4, string arg5, string arg6,
+            string arg7, string arg8, string arg9)
+            => throw new InvalidOperationException($"{nameof(Concat)} cannot be called client side");
+
         public static void Register(ModelBuilder builder)
         {
             var methodInfos = typeof(MDbFunctions).GetMethods().Where(mi => mi.Name == nameof(Concat));

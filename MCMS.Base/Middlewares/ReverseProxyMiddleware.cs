8b30c0183e6bdfe2e6cf196a135a51084a3d51d3
TDR <tdr@tdrs.ro>
2021-03-20 18:42:42 +0200

    removed unused directives from ReverseProxyMiddleware

--- MCMS.Base/Middlewares/ReverseProxyMiddleware.cs
+++ MCMS.Base/Middlewares/ReverseProxyMiddleware.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using MCMS.Base.Helpers;
 using Microsoft.AspNetCore.Http;
 
 namespace MCMS.Base.Middlewares

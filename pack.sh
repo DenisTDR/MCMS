6278c13bc0d8219150440ca84a99172b52af0d8b
TDR <tdr@tdrs.ro>
2021-04-06 03:36:36 +0200

    improved gitlab ci/cd flow

--- pack.sh
+++ pack.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+if [ $# -eq 0 ] || [[ $1 != "publish-"* ]]; then
+  echo "Invalid git tag argument supplied. Usage: pack.sh publish-x.y.z"
+  exit 1
+fi
+
+version=${1#publish-}
+
 projects=("./MCMS.Base/MCMS.Base" "./MCMS/MCMS" "./MCMS.Auth/MCMS.Auth" "./MCMS.Emailing/MCMS.Emailing" "./MCMS.Common/MCMS.Common" "./MCMS.Files/MCMS.Files")
 
-buildDir=./nuget-build
+buildDir=nuget-build
 
-version=0.0.2
+export VERSION=$version
+export ENV_TYPE=CI_BUILD
+rm -rf "$buildDir"/*.nupkg "$buildDir"/*.snupkg
 
-#rm -rf "$buildDir"/*.nupkg "$buildDir"/*.snupkg
+mkdir -p $buildDir
+dotnet nuget add source "$(pwd)/$buildDir" -n "Temporary build dir" || exit 1
 
 for i in "${projects[@]}"; do
-  dotnet pack -c Release -o "$buildDir" -p:PackageVersion=$version "$i".csproj
+  printf "\n Packing %s...\n" "$i"
+  dotnet pack -c Release -o "$buildDir" -p:PackageVersion="$version" "$i".csproj
 done
 
+dotnet nuget remove source "Temporary build dir" || exit 1

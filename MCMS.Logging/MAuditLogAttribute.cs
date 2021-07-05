c65aca4fcdd534443f948e87a091428891aa2d75
TDR <tdr@tdrs.ro>
2021-07-05 13:36:31 +0300

    v0.0.29-beta3: Updated Audit Logging Attribute

--- MCMS.Logging/MAuditLogAttribute.cs
+++ MCMS.Logging/MAuditLogAttribute.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,6 +10,13 @@ namespace MCMS.Logging
 {
     public class MAuditLogAttribute : ActionFilterAttribute
     {
+        private bool IgnoreReadOnlyRequests { get; }
+
+        public MAuditLogAttribute(bool ignoreReadOnlyRequests = true)
+        {
+            IgnoreReadOnlyRequests = ignoreReadOnlyRequests;
+        }
+
         private IMAuditLogger<MAuditLogAttribute> _auditLogger;
 
         public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
@@ -17,29 +26,46 @@ namespace MCMS.Logging
             if (next == null)
                 throw new ArgumentNullException(nameof(next));
 
-            await OnActionExecutingAsync(context);
+            OnActionExecuting(context);
             if (context.Result == null)
             {
                 OnActionExecuted(await next());
             }
         }
 
-        public async Task OnActionExecutingAsync(ActionExecutingContext context)
+        public override void OnActionExecuting(ActionExecutingContext context)
         {
+            if (ShouldSkipLog(context.HttpContext.Request))
+                return;
+
             _auditLogger = context.HttpContext.RequestServices.GetRequiredService<IMAuditLogger<MAuditLogAttribute>>();
             var request = context.HttpContext.Request;
             var data = new Dictionary<string, object>
             {
-                {"actionArguments", context.ActionArguments}, {"method", request.Method}
+                {"method", request.Method}
             };
+            if (context.ActionArguments.Any())
+            {
+                data["actionArguments"] = context.ActionArguments;
+            }
 
             _auditLogger.Log(data);
         }
 
         public override void OnActionExecuted(ActionExecutedContext context)
         {
+            if (ShouldSkipLog(context.HttpContext.Request))
+                return;
+
             var data = new Dictionary<string, object> {["statusCode"] = context.HttpContext.Response.StatusCode};
             _auditLogger.UpdateLog(data);
+        }
+
+        private static readonly string[] NonReadonlyHttpMethods = {"POST", "PUT", "DELETE", "PATCH"};
+
+        private bool ShouldSkipLog(HttpRequest request)
+        {
+            return !IgnoreReadOnlyRequests || NonReadonlyHttpMethods.Contains(request.Method);
         }
     }
 }

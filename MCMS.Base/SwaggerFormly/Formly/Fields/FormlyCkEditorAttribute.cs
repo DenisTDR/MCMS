abb46e29e8b823a30ed0e98d7319b1db38b2edc3
TDR <tdr@tdrs.ro>
2022-10-19 22:59:35 +0300

    ckeditor field with defined image upload url

--- MCMS.Base/SwaggerFormly/Formly/Fields/FormlyCkEditorAttribute.cs
+++ MCMS.Base/SwaggerFormly/Formly/Fields/FormlyCkEditorAttribute.cs
@@ -1,9 +1,25 @@
+using MCMS.Base.Helpers;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.OpenApi.Any;
+
 namespace MCMS.Base.SwaggerFormly.Formly.Fields
 {
     public class FormlyCkEditorAttribute : FormlyCustomFieldFieldAttribute
     {
         public FormlyCkEditorAttribute() : base("ckeditor")
         {
+        }
+
+        public override OpenApiObject GetCustomOpenApiConfig(LinkGenerator linkGenerator)
+        {
+            var obj = new OpenApiObject
+            {
+                ["imageUploadUrl"] = new OpenApiString(
+                    Env.Get("ROUTE_PREFIX") +
+                    linkGenerator.GetPathByAction("UploadCkEditor", "FilesUpload")?.Substring(1)),
+            };
+
+            return obj;
         }
     }
 }

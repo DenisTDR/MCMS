4dc39ce7ea335eb13b388c24ec47a60deb8dea48
Denis TDR <tdr@tdrs.ro>
2023-02-19 23:10:52 +0200

    swagger ui theme toggler

--- MCMS/wwwroot/api/docs/swagger-ui-theme.js
+++ MCMS/wwwroot/api/docs/swagger-ui-theme.js
@@ -1,19 +1,44 @@
-var alwaysDarkMode = true;
+const swaggerUiTheme = {
+    isDark: false,
+    init: () => {
+        let dark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+        if (!dark) {
+            dark = localStorage.getItem('dark-mode');
+        }
+        swaggerUiTheme.update(dark);
 
-function doDarkIfNeeded() {
-    var now = new Date();
-    var body = document.body;
-    if (alwaysDarkMode || now.getHours() < 7 || now.getHours() >= 18) {
-        body.classList.add("dark")
-    } else {
-        body.classList.remove("dark")
-    }
-
-    if (!alwaysDarkMode) {
-        setTimeout(function () {
-            doDarkIfNeeded();
-        }, 10 * 60 * 1000);
-    }
+        swaggerUiTheme.bindToggleButton();
+    },
+    update: (dark) => {
+        if (dark) {
+            document.body.classList.add("dark");
+            localStorage.setItem('dark-mode', 1);
+        } else {
+            document.body.classList.remove("dark");
+            localStorage.removeItem('dark-mode');
+        }
+        swaggerUiTheme.isDark = dark;
+    },
+    bindToggleButton: () => {
+        const topBar = document.querySelector('.topbar-wrapper');
+        if (!topBar) {
+            setTimeout(() => {
+                swaggerUiTheme.bindToggleButton();
+            }, 250);
+            return;
+        }
+        const button = document.createElement('button');
+        button.innerHTML = swaggerUiTheme.isDark ? '☀️' : '🌙';
+        button.classList.add('btn');
+        button.classList.add('theme-toggle');
+        button.addEventListener('click', () => {
+            console.log('click');
+            swaggerUiTheme.update(!swaggerUiTheme.isDark);
+            button.innerHTML = swaggerUiTheme.isDark ? '☀️' : '🌙';
+        });
+        topBar.append(button);
+    },
 }
 
-window.addEventListener('load', doDarkIfNeeded);
+
+window.addEventListener('load', swaggerUiTheme.init);

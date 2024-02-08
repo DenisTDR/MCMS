791335a44f49589bf34c0f9be788c44f17ac0e61
Denis TDR <tdr@tdrs.ro>
2024-02-08 15:03:22 +0200

    fixed turnstile error codes json field

--- MCMS.Common/Turnstile/TurnstileResponseModel.cs
+++ MCMS.Common/Turnstile/TurnstileResponseModel.cs
@@ -1,9 +1,11 @@
+using Newtonsoft.Json;
+
 namespace MCMS.Common.Turnstile
 {
     public class TurnstileResponseModel
     {
         public bool Success { get; set; }
-        public string[] ErrorCodes { get; set; }
+        [JsonProperty("error-codes")] public string[] ErrorCodes { get; set; }
         public string[] Messages { get; set; }
     }
 }

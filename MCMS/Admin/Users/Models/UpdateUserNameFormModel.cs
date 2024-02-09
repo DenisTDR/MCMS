8da82ede104b79c314de67f3b14b67ca4731108f
Denis TDR <tdr@tdrs.ro>
2024-02-09 19:17:24 +0200

    update user username as admin

---
+++ MCMS/Admin/Users/Models/UpdateUserNameFormModel.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using MCMS.Base.Data.FormModels;
+using MCMS.Base.SwaggerFormly.Formly.Fields;
+
+namespace MCMS.Admin.Users.Models
+{
+    public class UpdateUserNameFormModel : IFormModel
+    {
+        [FormlyField(ClassName = "col-12", Disabled = true)]
+        public string OldUserName { get; set; }
+
+        [Required]
+        [FormlyField(ClassName = "col-12")]
+        public string NewUserName { get; set; }
+    }
+}

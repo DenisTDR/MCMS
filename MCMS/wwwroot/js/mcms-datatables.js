2db43a6b6b920ab42cc25beb02c0ec1410aedd69
Denis TDR <tdr@tdrs.ro>
2024-02-16 19:52:51 +0200

    added reloadFirst method on mcmsTables

--- MCMS/wwwroot/js/mcms-datatables.js
+++ MCMS/wwwroot/js/mcms-datatables.js
@@ -10,6 +10,10 @@ const mcmsTables = [];
         if (this.length === 0) return;
         this[this.length - 1].ajax.reload();
     };
+    mcmsTables.reloadFirst = function () {
+        if (this.length === 0) return;
+        this[0].ajax.reload();
+    };
     window.mcmsDatatables = {
         bindDefaultDataTables: function (selector, initialConfig, actionsColumnContent, lang) {
             const tableId = selector.replace("#", "");

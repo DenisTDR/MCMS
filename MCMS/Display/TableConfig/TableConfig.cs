cbcb59e371fd361b9a5ded7769cc1493890e7dbf
Denis TDR <tdr@tdrs.ro>
2024-04-10 22:20:59 +0300

    value for data field on index column

--- MCMS/Display/TableConfig/TableConfig.cs
+++ MCMS/Display/TableConfig/TableConfig.cs
@@ -70,7 +70,7 @@ namespace MCMS.Display.TableConfig
             if (HasTableIndexColumn)
             {
                 columns = columns.Prepend(new TableColumn
-                    { Name = "#", ClassName = "non-toggleable", Data = ServerSide ? "_index" : null });
+                    { Name = "#", ClassName = "non-toggleable", Data = "_index" });
             }
 
             if (CheckboxSelection)
